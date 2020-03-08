8dd868d6dde09d830373fa6e9114090e8f6b57d2
LeoPFreitas <leonardo.cadu2@hotmail.com>
2020-03-07 22:25:08 -0300

    (feat) user register

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,10 @@
 import { Router } from 'express';
 import User from './app/models/User';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Leonardo Freitas',
-    email: 'leo@hotmail.com',
-    password_hash: '123456',
-  });
-
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
