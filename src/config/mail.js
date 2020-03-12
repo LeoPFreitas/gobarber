712bb676744bbd86b51e13cc95bdcd7f12221005
LeoPFreitas <leonardo.cadu2@hotmail.com>
2020-03-12 18:28:28 -0300

    (feat) send email on appointment cancellation

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '1f661a58067664',
+    pass: '461e69b5c23c3d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
